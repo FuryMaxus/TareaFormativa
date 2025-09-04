b479f8c74b5a7053d5621851c2a060a340a669c6
furymaxus <joi.valenzuela@duocuc.cl>
2025-09-03 22:15:15 -0400

    avance en las funciones del main

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,7 +1,9 @@
 package org.example
 
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.runBlocking
 import org.example.model.Cliente
+import org.example.model.EstadoPedido
 import org.example.model.Pedido
 import org.example.model.Producto
 import org.example.model.TipoCliente
@@ -37,39 +39,88 @@ fun pedirDatosCliente(): Cliente
     return Cliente(nombre,tipoCliente)
 }
 
-fun
-
-fun mostrarCatalogoFiltradoPorCategoria(categoria: String,
-                                        catalogo: List<Producto>): List<Producto>
+fun obtenerCategorias(catalogo: List<Producto>): MutableMap<Int,String>
 {
-    val catalogoFiltrado: List<Producto> = catalogo.filter { it.categoria == categoria }
-    println("Catalogo de $categoria")
-    for( (index,producto) in catalogoFiltrado.withIndex()) {
-        println("$index. $producto")
+    val mapCategorias: MutableMap<Int,String> = mutableMapOf()
+    catalogo.forEachIndexed { i,value ->
+        if (value.categoria !in mapCategorias.values){
+            mapCategorias.put(i+1, value.categoria)
+        }
     }
-    return catalogoFiltrado
-    //RECORDATORIO: no tengo idea si hay que solo mostrarlo o esto cambia como se elijen los productos
+    return mapCategorias
 }
 
-fun agregarProducto(catalogo: List<Producto>,
-                    pedido: Pedido){
-    val opcionProducto = when (pedirDato(
-        "Quieres agregar por id o por nombre?\n1.Id\n2.Nombre",
-        {it in listOf("1","2") } )
-    ){
-        "1" -> {
-            do {
+fun escogerCategoria(catalogo: List<Producto>,categorias: MutableMap<Int,String>): Int
+{
+    val builderOpcionesCategorias: StringBuilder = StringBuilder("Elige que categoria de producto desea ver\n0. Cancelar")
 
-            }
-
-        }
-        "2" -> {
-
-        }
-         else -> {}
+    for((key,value) in categorias){
+        builderOpcionesCategorias.append("${key}. $value\n")
     }
+
+    val textoOpcionesCategorias: String = builderOpcionesCategorias.toString()
+
+    val opcionCategoriaEscogida: Int = pedirDato(textoOpcionesCategorias) {
+        val num = it.toIntOrNull(); it.isNotBlank() && (num in categorias.keys ||num == 0)
+    }.toInt()
+
+    return opcionCategoriaEscogida
 }
 
+fun mostrarCatalogo(catalogo: List<Producto>)
+{
+    val categorias: MutableMap<Int,String> = obtenerCategorias(catalogo)
+
+    val opcionCategoriaEscogida: Int = escogerCategoria(catalogo,categorias)
+
+    if (opcionCategoriaEscogida == 0) return
+
+    val categoriaSeleccionada: String = categorias.getValue(opcionCategoriaEscogida)
+
+    val catalogofiltrado: List<Producto> = catalogo.filter { it.categoria == categoriaSeleccionada }
+
+    println("Catalogo filtrado por $categoriaSeleccionada")
+    println("ID\tNOMBRE\tPRECIO")
+    catalogofiltrado.forEach { println("${it.id}\t${it.nombre}\t${it.precio}") }
+
+}
+
+fun añadirProducto(pedido: Pedido,catalogo: List<Producto>) {
+    print("Ingrese el id del producto que desee agregar a su orden: ")
+    val idProducto: String = readLine()?.trim().orEmpty()
+    if(idProducto.isEmpty()){
+        println("No se encontro el producto que ingreso, para ver los productos revise el catalogo")
+        return
+    }
+    pedido.listaProductos.add(catalogo[idProducto.toInt()])
+}
+
+fun aplicarDescuentosYpromociones(){
+    TODO("Aun no se muy bien a que se refiere esto, deai veo")
+}
+
+fun errorPedido(pedido: Pedido){
+    pedido.estadoPedido = EstadoPedido.Error("Error en el proceso del pedido")
+}
+
+suspend fun procesarPedido(pedido: Pedido){
+    println("Pedido en preparacion...")
+    pedido.estadoPedido = EstadoPedido.EnPreparacion
+    delay(3000L)
+    println("Pedido listo para entregar")
+    pedido.estadoPedido = EstadoPedido.ListoParaEntrega
+    TODO("Siento que faltan hacer cositas aqui")
+}
+
+fun mostrarDesgloseFinal(pedido: Pedido){
+    println("Resultado de pedido:")
+    TODO("No se que datos hay que desglosar aparte de los nombres y precio, supongo que precio total")
+
+}
+
+fun menuPrincipal() {
+    TODO("menu con las 5 opciones de la guia")
+}
 fun main() = runBlocking {
-
+    menuPrincipal()
 }
